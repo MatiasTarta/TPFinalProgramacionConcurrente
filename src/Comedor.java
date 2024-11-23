8a49a7079e754ca712b361179d23efd05d42b8e4
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-11-23 20:42:22 -0300

    comedor arreglado, falta terminarlo

--- src/Comedor.java
+++ src/Comedor.java
@@ -1,4 +1,3 @@
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Semaphore;
 
@@ -10,12 +9,9 @@ public class Comedor {
 
     public Comedor() {
         totalMesas = 10;// maximo de mesas
-        sillas = 4;// constante,cantidad personas por mesa
+        sillas = 3;// constante,cantidad personas por mesa
         semaforoMesas = new Semaphore(totalMesas * sillas);
-        mesas = new BlockingQueue[totalMesas];
-        for (int i = 0; i < totalMesas; i++) {
-            mesas[i] = new ArrayBlockingQueue<>(sillas);
-        }
+
     }
 
     public boolean entrarComedor() throws InterruptedException {
@@ -31,27 +27,11 @@ public class Comedor {
     }
 
     public void sentarse() throws InterruptedException {
-        for (int i = 0; i < totalMesas; i++) {
-            if (mesas[i].remainingCapacity() > 0) { // Si hay espacio en la mesa
-                mesas[i].put(Thread.currentThread().getName()); // El hilo se sienta
-                System.out.println(Thread.currentThread().getName() + " se sentó en la mesa " + (i + 1));
 
-                if (mesas[i].size() == sillas) {
-                    System.out.println("Mesa " + (i + 1) + " está llena. Los hilos en esta mesa están comiendo.");
-                }
-                break; // Sale del bucle cuando se ha sentado en la mesa
-            }
-        }
     }
 
     public void levantarse() {
-        // busca en que mesa estaba el hilo y liberarlo
-        for (int i = 0; i < totalMesas; i++) {
-            if (mesas[i].remove(Thread.currentThread().getName())) {
-                System.out.println(Thread.currentThread().getName() + " se levantó de la mesa " + i + 1);
-                break; // Una vez liberado el hilo de la mesa, salir del bucle
-            }
-        }
+
     }
 
     public void salir() {
